0812e9c70791dd1c654bcc79f4a001154cbce02c
nadiazaleta <rhmanadya@email.com>
2025-10-04 22:00:41 +0800

    Tukar gambar Bila Kau Tak Disampingku dan Film Favorit

--- html/music-list.js
+++ html/music-list.js
@@ -2,25 +2,25 @@ let allMusic = [
   {
     name: "Bila Kau Tak Disampingku",
     artist: "Sheila On 7",
-    img: "images/music-3.jpeg",
-    src: "songs/Bila-Kau-Tak-Disampingku.mp3"
+    img: "./images/music-3.jpeg",  // ✅ dulu 1, sekarang jadi 3
+    src: "./songs/Bila-Kau-Tak-Disampingku.mp3"
   },
   {
     name: "Dan",
     artist: "Sheila On 7",
-    img: "images/music-2.jpeg",
-    src: "songs/Dan-sheila.mp3"
+    img: "./images/music-2.jpeg",
+    src: "./songs/Dan-sheila.mp3"
   },
   {
     name: "Film Favorit",
     artist: "Sheila On 7",
-    img: "images/music-1.jpeg",
-    src: "songs/Film-favorit.mp3"
+    img: "./images/music-1.jpeg",  // ✅ dulu 3, sekarang jadi 1
+    src: "./songs/Film-favorit.mp3"
   },
   {
     name: "Itu Aku",
     artist: "Sheila On 7",
-    img: "images/music-4.jpeg",
-    src: "songs/Itu-aku.mp3"
+    img: "./images/music-4.jpeg",
+    src: "./songs/Itu-aku.mp3"
   }
 ];
