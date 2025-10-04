a7bf464b8c0fe6c432df7bbcb5ab4c8f00cb1670
miawwmii <nanadjjk9@gmail.com>
2025-10-04 21:27:20 +0800

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Bila Kau Tak Disampingku",
+    artist: "Sheila On 7",
+    img: "images/music-3.jpeg",
+    src: "songs/Bila-Kau-Tak-Disampingku.mp3"
+  },
+  {
+    name: "Dan",
+    artist: "Sheila On 7",
+    img: "images/music-2.jpeg",
+    src: "songs/Dan-sheila.mp3"
+  },
+  {
+    name: "Film Favorit",
+    artist: "Sheila On 7",
+    img: "images/music-1.jpeg",
+    src: "songs/Film-favorit.mp3"
+  },
+  {
+    name: "Itu Aku",
+    artist: "Sheila On 7",
+    img: "images/music-4.jpeg",
+    src: "songs/Itu-aku.mp3"
+  }
+];
