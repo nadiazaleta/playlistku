968f1ed4d3e1a88dfbff3bb7b0a1a4f65a949f05
nadiazaleta <rhmanadya@email.com>
2025-10-04 21:38:22 +0800

    Initial commit dari VS Code

---
+++ html/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Bila Kau Tak Disampingku",
+    artist: "Sheila On 7",
+    img: "music-1.jpeg",
+    src: "Bila-Kau-Tak-Disampingku.mp3"
+  },
+  {
+    name: "Dan",
+    artist: "Sheila On 7",
+    img: "music-2.jpeg",
+    src: "Dan-sheila.mp3"
+  },
+  {
+    name: "Film Favorit",
+    artist: "Sheila On 7",
+    img: "music-3.jpeg",
+    src: "Film-favorit.mp3"
+  },
+  {
+    name: "Itu Aku",
+    artist: "Sheila On 7",
+    img: "music-4.jpeg",
+    src: "Itu-aku.mp3"
+  }
+];
